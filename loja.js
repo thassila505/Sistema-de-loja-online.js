95b7dbdeca77cd53aeefcde65033ef59813e7457
thassila505 <thassila.moreira@escola.pr.gov.br>
2026-03-25 15:03:23 +0000

    2

--- loja.js
+++ loja.js
@@ -1,6 +1,24 @@
-class produto{}
+class produto{
+    constructor(nome, preco){
+        this.preco = preco
+    }
 
-class carrinho{}
+}
+
+class carrinho{
+    constructor(){
+        this.produtos = []
+    }
+    adicionarProdutos(produtos){
+        this.produtos.push(produto)
+    }
+    listarProdutos (){
+        return this.produtos
+    }
+    calcularTotal(){
+        return this.produtos.reduce((total, p) => total + p.preco, 0)
+    }
+}
 
 class cliente{}
 
